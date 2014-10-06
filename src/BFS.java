b0a4cdde70a05eb2ccdd003e967a3e704714f296
Ruxaxup <cavernasboy@gmail.com>
2014-10-06 09:38:09 -0500

    Pendientes condition added at BFS.java

--- src/BFS.java
+++ src/BFS.java
@@ -7,39 +7,39 @@ import java.util.Set;
 
 public class BFS {
 	private Grafo G;
+	
 	public BFS(Grafo G) {
 		this.G = G;
 	}
 	
-        
-        
-	public boolean buscar(char c){
+    public boolean buscar(char c){
 		Queue<Nodo> pendientes = new LinkedList<>();
 		Set<Nodo> visitados = new HashSet<>();
 		//Metemos el nodo raiz a la lista
 		pendientes.add(G.getRoot());
-                /*El ciclo se repite hasta que se hayan visitado todos los nodos
-                  o se haya encontrado el elemento*/
+        /*El ciclo se repite hasta que se hayan visitado todos los nodos
+          o se haya encontrado el elemento*/
 		while(!pendientes.isEmpty()){            
             MainFrame.out.append("Cola: "+pendientes.toString());
             MainFrame.out.append("\nVisitados: "+visitados.toString());            
             //Sacamos el elemento de la cabeza de la Cola
             Nodo V = pendientes.poll();
+            System.out.println(V+" ");
 			MainFrame.out.append("\nNodo Actual: "+V);                        	
-                        if(esIgual(V,c)){		
-                            //Si es el elemento, cerramos la busqueda
-                            MainFrame.out.append("\n===============================\n");
+            if(esIgual(V,c)){		
+                //Si es el elemento, cerramos la busqueda
+                MainFrame.out.append("\n===============================\n");
 				return true;
 			}else{
-                                //Agregamos el nodo al conjunto de nodos visitados
+                //Agregamos el nodo al conjunto de nodos visitados
 				visitados.add(V);
-                                //Obtenemos todos los nodos vecinos del nodo actual
+                //Obtenemos todos los nodos vecinos del nodo actual
 				List<Nodo> adyacentes = V.getAdyacentes();
 				MainFrame.out.append("\nVecinos de "+V+": "+adyacentes.toString());
 				for (Nodo nodo : adyacentes) {
-                                    //Agregamos los vecinos a la Cola excepto 
-                                    //los que están en el conjunto de los visitados
-					if(!visitados.contains(nodo))
+                    /*Agregamos los vecinos a la Cola excepto 
+                      los que están en el conjunto de los visitados*/
+					if(!visitados.contains(nodo) && !pendientes.contains(nodo))
 						pendientes.add(nodo);
 				}
 			}
@@ -49,7 +49,6 @@ public class BFS {
 	}
 	int contador = 0;
 	public boolean esIgual(Nodo n, char c){
-		contador++;
 		return n.getC()==c;
 	}
 	
--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,4 +1,3 @@
-
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
@@ -20,138 +19,128 @@ import javax.swing.JTextField;
  *
  * @author Ruxaxup
  */
-public class MainFrame extends JFrame implements ActionListener{
-    private int w_width, w_height;
-    public static JTextArea out;
-    private JButton bStart;
-    private JTextField tfElement;
-    
-    
-    private Grafo G;
-    
-    public MainFrame(){
-        super("Busqueda en Anchura");
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        initComponents();
-        pack();
-    }
+public class MainFrame extends JFrame implements ActionListener {
+	private int w_width, w_height;
+	public static JTextArea out;
+	private JButton bStart;
+	private JTextField tfElement;
 
-    private void initComponents() {
-        //Colocacion y definicion de tamaño
-        Dimension windowDimension = Toolkit.getDefaultToolkit().getScreenSize(); 
-        w_width = 400;
-        w_height = (int)(windowDimension.height * 0.9);
-        setPreferredSize(new Dimension(w_width,w_height));
-        setLocation((windowDimension.width / 2) - (w_width/2),
-                    (windowDimension.height / 2) - (w_height/2));
-        
-        //Creacion de la salida
-        out = new JTextArea();
-        out.setEditable(false);
-        Font fOut = new Font("Arial", Font.PLAIN, 20);
-        out.setFont(fOut);
-        JScrollPane spOut = new JScrollPane(
-        		out, 
-        		JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
-        		JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-        
-        //Creacion componentes
-        bStart = new JButton("Start");
-        bStart.addActionListener(this);
-        
-        //--Panel Buscar
-        JPanel panelBuscar = new JPanel(new FlowLayout(FlowLayout.LEFT));
-        tfElement = new JTextField(5);        
-        tfElement.addKeyListener(new LimitadorCaracter(tfElement, 1));
-        panelBuscar.add(new JLabel("Elemento a buscar:"));
-        panelBuscar.add(tfElement);
-        panelBuscar.add(bStart);
-        
-        //Colocacion de componentes
-        setLayout(new GridBagLayout());
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.fill = GridBagConstraints.BOTH;
-        gbc.gridheight = 3;
-        gbc.gridwidth = 3;
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        gbc.weightx = 3;
-        gbc.weighty = 3;
-        //Agregamos la salida
-        add(spOut, gbc);
-        
-        gbc.gridheight = 1;
-        gbc.gridwidth = 3;
-        gbc.gridx = 0;
-        gbc.gridy = 3;
-        gbc.weightx = 0;
-        gbc.weighty = 0;
-        //Agregamos el panel de busqueda
-        add(panelBuscar,gbc);
-           
-    }
-    
-    private void creaGrafo(){
-    	G = new Grafo();
-    	//CREAMOS LOS NODOS
+	private Grafo G;
+
+	public MainFrame() {
+		super("Busqueda en Anchura");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		initComponents();
+		pack();
+	}
+
+	private void initComponents() {
+		// Colocacion y definicion de tamaño
+		Dimension windowDimension = Toolkit.getDefaultToolkit().getScreenSize();
+		w_width = 400;
+		w_height = (int) (windowDimension.height * 0.9);
+		setPreferredSize(new Dimension(w_width, w_height));
+		setLocation((windowDimension.width / 2) - (w_width / 2),
+				(windowDimension.height / 2) - (w_height / 2));
+
+		// Creacion de la salida
+		out = new JTextArea();
+		out.setEditable(false);
+		Font fOut = new Font("Arial", Font.PLAIN, 20);
+		out.setFont(fOut);
+		JScrollPane spOut = new JScrollPane(out,
+				JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+				JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+
+		// Creacion componentes
+		bStart = new JButton("Start");
+		bStart.addActionListener(this);
+
+		// --Panel Buscar
+		JPanel panelBuscar = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		tfElement = new JTextField(5);
+		tfElement.addKeyListener(new LimitadorCaracter(tfElement, 1));
+		panelBuscar.add(new JLabel("Elemento a buscar:"));
+		panelBuscar.add(tfElement);
+		panelBuscar.add(bStart);
+
+		// Colocacion de componentes
+		setLayout(new GridBagLayout());
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.fill = GridBagConstraints.BOTH;
+		gbc.gridheight = 3;
+		gbc.gridwidth = 3;
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.weightx = 3;
+		gbc.weighty = 3;
+		// Agregamos la salida
+		add(spOut, gbc);
+
+		gbc.gridheight = 1;
+		gbc.gridwidth = 3;
+		gbc.gridx = 0;
+		gbc.gridy = 3;
+		gbc.weightx = 0;
+		gbc.weighty = 0;
+		// Agregamos el panel de busqueda
+		add(panelBuscar, gbc);
+
+	}
+
+	private void creaGrafo() {
+		G = new Grafo();
+		// CREAMOS LOS NODOS
 		G.addNodo(new Nodo('A'));
 		G.addNodo(new Nodo('B'));
 		G.addNodo(new Nodo('C'));
 		G.addNodo(new Nodo('D'));
-		G.addNodo(new Nodo('E'));
-		G.addNodo(new Nodo('F'));
-		G.addNodo(new Nodo('G'));
-		G.addNodo(new Nodo('H'));
-		G.addNodo(new Nodo('I'));
-		G.addNodo(new Nodo('J'));
-		G.addNodo(new Nodo('K'));
+		G.addNodo(new Nodo('R'));
 		
-		//CREAMOS LOS ENLACES
+
+		// CREAMOS LOS ENLACES
+		G.createEdge('R', 'A');
+		G.createEdge('R', 'B');
 		G.createEdge('A', 'B');
 		G.createEdge('A', 'C');
-		G.createEdge('B', 'D');
-		G.createEdge('B', 'E');
-		G.createEdge('B', 'H');
-		G.createEdge('C', 'F');
-		G.createEdge('C', 'G');
-		G.createEdge('C', 'I');
-		G.createEdge('H', 'J');
-		G.createEdge('I', 'K');
+		G.createEdge('B', 'C');
+		G.createEdge('C', 'D');
 		
-		//ESTABLECEMOS EL NODO RAIZ
-		G.setRoot('A');
-    }
+		// ESTABLECEMOS EL NODO RAIZ
+		G.setRoot('R');
+		//G.setRoot('D');
+	}
 
-    private void BFS(){
-    	//BFS (BUSQUEDA EN AMPLITUD)
-        Thread t = new Thread(new Runnable(){
+	private void BFS() {
+		// BFS (BUSQUEDA EN AMPLITUD)
+		Thread t = new Thread(new Runnable() {
 
-            @Override
-            public void run() {
-                BFS bfs = new BFS(G);
-                char elementoABuscar = tfElement.getText().charAt(0);
-                elementoABuscar = Character.toUpperCase(elementoABuscar);
-                if(bfs.buscar(elementoABuscar)){
-                    out.append("-- Se encontró: "+elementoABuscar+" --");
-                }else{                        
-                        out.append("-- No se encontró: "+elementoABuscar+" --");
-                }
-            }
-            
-        });
-        t.start();
-    }
-    
+			@Override
+			public void run() {
+				BFS bfs = new BFS(G);
+				char elementoABuscar = tfElement.getText().charAt(0);
+				elementoABuscar = Character.toUpperCase(elementoABuscar);
+				if (bfs.buscar(elementoABuscar)) {
+					out.append("-- Se encontró: " + elementoABuscar + " --");
+				} else {
+					out.append("-- No se encontró: " + elementoABuscar + " --");
+				}
+			}
+
+		});
+		t.start();
+	}
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if(e.getSource() == bStart){
+		if (e.getSource() == bStart) {
 			out.setText("");
-			if(tfElement.getText().isEmpty()){
+			if (tfElement.getText().isEmpty()) {
 				tfElement.setText("A");
 			}
 			creaGrafo();
 			BFS();
 		}
-		
+
 	}
 }
